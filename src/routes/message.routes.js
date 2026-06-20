a9931852a84abc8c659f9be1124e28ebc07b523f
Sunny Kumar <sk7522422@email.com>
2026-06-20 20:11:18 +0530

    2006- remove extra api  for audio message

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import { sendAudioMessage, sendFile } from '../controllers/message.controller.js';
+import { sendFile } from '../controllers/message.controller.js';
 import { authenticateUser } from '../middlewares/auth.middleware.js';
 import { uploadAudio, uploadFile } from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
-router
-  .route('/send-audio-message')
-  .post(authenticateUser, uploadAudio.single('audioBlob'), sendAudioMessage);
 router.route('/send-file').post(authenticateUser, uploadFile.single('fileBlob'), sendFile);
 
 export default router;
