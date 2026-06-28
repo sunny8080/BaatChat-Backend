7502060d3107cdee3389c99170788ce3018ad09f
Sunny Kumar <sk7522422@email.com>
2026-06-29 03:15:04 +0530

    2906- update mailer port

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -64,6 +64,7 @@ export const mailSender = async (toEmail, subject, html, text) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: process.env.NODE_ENV !== 'development',
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
